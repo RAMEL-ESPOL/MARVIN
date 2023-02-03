a3353dcc193e47b58efe863b4eae9b39c5a91c25
lap31196 <74328912+lap31196@users.noreply.github.com>
2023-02-03 16:01:04 -0500

    Update Readme.m

--- catkin_ws_vehiculo/Readme.m
+++ catkin_ws_vehiculo/Readme.m
@@ -1,2 +1,3 @@
-catkin_ws_vehiculo : Paqueteria para controlar la aceleracion, giro y los sensores (LIDAR, Intel RealSense, Camaras IMX) en el ambiente virtual de GAZEBO
+catkin_ws_vehiculo : Paqueteria para controlar la aceleracion, giro y los sensores (LIDAR, Intel RealSense, Camaras IMX) en el ambiente virtual de GAZEBO.
+
 
